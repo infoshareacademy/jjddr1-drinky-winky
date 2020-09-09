994bd7ddc9bae78e8f0ac84ee7b5f18bcdeec4a7
Daniel.Laga <daniellaga84@gmail.com>
2020-09-09 14:19:15 +0200

    Add Maven settings + Logger

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
+
+    public static void main(String[] args) {
+
+    }
+}
