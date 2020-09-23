b3fdb39a9c8d708213cf4ca38736b03c0cb0b25a
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-09-23 23:15:37 +0200

    Menu done, links to specific drinks to do, colors corrected

--- src/main/java/com/infoshare/drinkywinky/drinkslibrary/ListOfDrinks.java
+++ src/main/java/com/infoshare/drinkywinky/drinkslibrary/ListOfDrinks.java
@@ -29,37 +29,37 @@ public class ListOfDrinks {
             STDOUT.info("\n");
             STDOUT.info("┌──────────────────────────────────────────┐\n");
             STDOUT.info("│ \u001b[33m CHOOSE NUMBER OF DRINK OR OTHER OPTION \u001b[0m │\n");
-            STDOUT.info("│           \u001b[31m \u001b[0m                              │\n");
-            STDOUT.info("│   X. BACK TO THE MAIN MENU               │\n");
             STDOUT.info("│                                          │\n");
             for (int i = (1 + pageNumber * NUMBER_OF_DRINKS_BY_PAGE); i <= (NUMBER_OF_DRINKS_BY_PAGE + (pageNumber * NUMBER_OF_DRINKS_BY_PAGE)); i++) {
                 if (i <= defaultList.size()) {
                     //TODO Magic numbers
                     int numberOfSpaces = 37 - Integer.toString(i).length() - alphabeticalList.get(i - 1).length();
                     String whitespace = String.format("%1$" + numberOfSpaces + "s", "");
-                    STDOUT.info("│   {}. {}{}│\n", i, alphabeticalList.get(i - 1), whitespace);
+                    STDOUT.info("│   \u001b[33m{}.\u001b[0m {}{}│\n", i, alphabeticalList.get(i - 1), whitespace);
                 } else {
                     int numberOfSpaces2 = 33 - Integer.toString(i).length();
                     String whitespace = String.format("%1$" + numberOfSpaces2 + "s", "");
-                    STDOUT.info("│   {}. --- {}│\n", i, whitespace);
+                    STDOUT.info("│   \u001b[33m{}.\u001b[0m --- {}│\n", i, whitespace);
                 }
             }
             STDOUT.info("│                                          │\n");
-            STDOUT.info("│                                          │\n");
             STDOUT.info("│                Page {}/{}                  │\n", pageNumber + 1, numberOfPages);
             if (numberOfPages > 2 && pageNumber != 0 && pageNumber != numberOfPages - 1) {
-                STDOUT.info("│   P <- Previous page    Next page -> N   │\n");
+                STDOUT.info("│   \u001b[33mP\u001b[0m <- Previous page    Next page -> \u001b[33mN\u001b[0m   │\n");
             }
             if (pageNumber == numberOfPages - 1 && numberOfPages>1) {
-                STDOUT.info("│   P <- Previous page                     │\n");
+                STDOUT.info("│   \u001b[33mP\u001b[0m <- Previous page                     │\n");
             }
             if (pageNumber == 0  && numberOfPages>1) {
-                STDOUT.info("│                         Next page -> N   │\n");
+                STDOUT.info("│                         Next page -> \u001b[33mN\u001b[0m   │\n");
             }
+            STDOUT.info("│                                          │\n");
+            STDOUT.info("│                                          │\n");
+            STDOUT.info("│    Press \u001b[33mX\u001b[0m to return to the MAIN MENU    │\n");
             STDOUT.info("└──────────────────────────────────────────┘\n");
 
             STDOUT.info("\n");
-            STDOUT.info("CHOOSE NUMBER OF DRINK, CHANGE PAGE OR GO TO THE MAIN MENU: ");
+            STDOUT.info("\u001b[33mYOUR CHOICE: \u001b[0m");
             chooseTheOption();
         } while (true);
     }
