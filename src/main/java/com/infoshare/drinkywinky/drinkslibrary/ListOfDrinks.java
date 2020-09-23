6452cb1ac51344bcc05359fe8a9650d3c914963c
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-09-23 16:52:35 +0200

    Menu pages done

--- src/main/java/com/infoshare/drinkywinky/drinkslibrary/ListOfDrinks.java
+++ src/main/java/com/infoshare/drinkywinky/drinkslibrary/ListOfDrinks.java
@@ -11,33 +11,38 @@ import java.util.stream.Collectors;
 public class ListOfDrinks {
     public static final Scanner SCANNER = new Scanner(System.in);
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
+    public static final int NUMBER_OF_DRINKS_BY_PAGE = 5;
     private int pageNumber = 0;
 
     public void alphabeticalMenu() {
         List<String> defaultList = Utils.getNamesOfAllDrink();
         List<String> alphabeticalList = defaultList.stream().sorted().collect(Collectors.toList());
-        int numberOfPages = defaultList.size()%5+1;
+        int numberOfPages = defaultList.size() / NUMBER_OF_DRINKS_BY_PAGE + 1;
 
         STDOUT.info("\n");
         STDOUT.info("┌──────────────────────────────────────────┐\n");
-        STDOUT.info("│                                          │\n");
         STDOUT.info("│ \u001b[33m CHOOSE NUMBER OF DRINK OR OTHER OPTION \u001b[0m │\n");
         STDOUT.info("│           \u001b[33m \u001b[0m                              │\n");
-        for (int i = (1 + pageNumber * 5); i <= (5 + pageNumber * 5); i++) {
-            int numberOfSpaces=37-Integer.toString(i).length()-alphabeticalList.get(i - 1).length();
-            String whitespace = String.format("%1$"+numberOfSpaces+"s", "");
-            STDOUT.info("│   {}. {}{}│\n", i, alphabeticalList.get(i - 1),whitespace);
+        STDOUT.info("│   X. BACK TO THE MAIN MENU               │\n");
+        STDOUT.info("│                                          │\n");
+        for (int i = (1 + pageNumber * NUMBER_OF_DRINKS_BY_PAGE); i <= (NUMBER_OF_DRINKS_BY_PAGE + (pageNumber * NUMBER_OF_DRINKS_BY_PAGE)); i++) {
+            if (i <= defaultList.size()) {
+                int numberOfSpaces = 37 - Integer.toString(i).length() - alphabeticalList.get(i - 1).length();
+                String whitespace = String.format("%1$" + numberOfSpaces + "s", "");
+                STDOUT.info("│   {}. {}{}│\n", i, alphabeticalList.get(i - 1), whitespace);
+            } else {
+                int numberOfSpaces2 = 33 - Integer.toString(i).length();
+                String whitespace = String.format("%1$" + numberOfSpaces2 + "s", "");
+                STDOUT.info("│   {}. --- {}│\n", i, whitespace);
+            }
         }
         STDOUT.info("│                                          │\n");
-        STDOUT.info("│   N -> go to the NEXT PAGE               │\n");
-        STDOUT.info("│   P -> go to the PREVIOUS PAGE           │\n");
-        STDOUT.info("│                                          │\n");
-        STDOUT.info("│  \u001b[36m 7. BACK TO MAIN MENU\u001b[0m                   │\n");
-        STDOUT.info("│                                          │\n");
-        STDOUT.info("│                                 Page {}/{} │\n",pageNumber+1,numberOfPages);
+        STDOUT.info("│                  Page {}/{}                │\n", pageNumber + 1, numberOfPages);
+        STDOUT.info("│   P <- Previous page    Next page -> N   │\n");
         STDOUT.info("└──────────────────────────────────────────┘\n");
+
         STDOUT.info("\n");
-        STDOUT.info("CHOOSE NUMBER: ");
+        STDOUT.info("CHOOSE NUMBER OF DRINK, CHANGE PAGE OR GO TO THE MAIN MENU: ");
 
     }
 
