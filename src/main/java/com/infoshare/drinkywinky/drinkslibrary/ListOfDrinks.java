12ff3786184b64b7f6368dae5520381165b52185
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-09-23 21:44:50 +0200

    Menu done, links to specific drinks to do

--- src/main/java/com/infoshare/drinkywinky/drinkslibrary/ListOfDrinks.java
+++ src/main/java/com/infoshare/drinkywinky/drinkslibrary/ListOfDrinks.java
@@ -1,54 +1,105 @@
 package com.infoshare.drinkywinky.drinkslibrary;
 
+import com.infoshare.drinkywinky.menu.Menu;
 import com.infoshare.drinkywinky.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
+import static com.infoshare.drinkywinky.menu.Menu.SCANNER;
+
 public class ListOfDrinks {
-    public static final Scanner SCANNER = new Scanner(System.in);
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
     public static final int NUMBER_OF_DRINKS_BY_PAGE = 5;
     private int pageNumber = 0;
+    private String in;
+    private int numberOfPages;
 
     public void alphabeticalMenu() {
         List<String> defaultList = Utils.getNamesOfAllDrink();
         List<String> alphabeticalList = defaultList.stream().sorted().collect(Collectors.toList());
-        int numberOfPages = defaultList.size() / NUMBER_OF_DRINKS_BY_PAGE + 1;
+        numberOfPages = defaultList.size() / NUMBER_OF_DRINKS_BY_PAGE;
+        if (defaultList.size() % NUMBER_OF_DRINKS_BY_PAGE != 0) {
+            numberOfPages = defaultList.size() / NUMBER_OF_DRINKS_BY_PAGE + 1;
+        }
 
-        STDOUT.info("\n");
-        STDOUT.info("┌──────────────────────────────────────────┐\n");
-        STDOUT.info("│ \u001b[33m CHOOSE NUMBER OF DRINK OR OTHER OPTION \u001b[0m │\n");
-        STDOUT.info("│           \u001b[33m \u001b[0m                              │\n");
-        STDOUT.info("│   X. BACK TO THE MAIN MENU               │\n");
-        STDOUT.info("│                                          │\n");
-        for (int i = (1 + pageNumber * NUMBER_OF_DRINKS_BY_PAGE); i <= (NUMBER_OF_DRINKS_BY_PAGE + (pageNumber * NUMBER_OF_DRINKS_BY_PAGE)); i++) {
-            if (i <= defaultList.size()) {
-                int numberOfSpaces = 37 - Integer.toString(i).length() - alphabeticalList.get(i - 1).length();
-                String whitespace = String.format("%1$" + numberOfSpaces + "s", "");
-                STDOUT.info("│   {}. {}{}│\n", i, alphabeticalList.get(i - 1), whitespace);
-            } else {
-                int numberOfSpaces2 = 33 - Integer.toString(i).length();
-                String whitespace = String.format("%1$" + numberOfSpaces2 + "s", "");
-                STDOUT.info("│   {}. --- {}│\n", i, whitespace);
+        do {
+            STDOUT.info("\n");
+            STDOUT.info("┌──────────────────────────────────────────┐\n");
+            STDOUT.info("│ \u001b[33m CHOOSE NUMBER OF DRINK OR OTHER OPTION \u001b[0m │\n");
+            STDOUT.info("│           \u001b[31m \u001b[0m                              │\n");
+            STDOUT.info("│   X. BACK TO THE MAIN MENU               │\n");
+            STDOUT.info("│                                          │\n");
+            for (int i = (1 + pageNumber * NUMBER_OF_DRINKS_BY_PAGE); i <= (NUMBER_OF_DRINKS_BY_PAGE + (pageNumber * NUMBER_OF_DRINKS_BY_PAGE)); i++) {
+                if (i <= defaultList.size()) {
+                    //TODO Magic numbers
+                    int numberOfSpaces = 37 - Integer.toString(i).length() - alphabeticalList.get(i - 1).length();
+                    String whitespace = String.format("%1$" + numberOfSpaces + "s", "");
+                    STDOUT.info("│   {}. {}{}│\n", i, alphabeticalList.get(i - 1), whitespace);
+                } else {
+                    int numberOfSpaces2 = 33 - Integer.toString(i).length();
+                    String whitespace = String.format("%1$" + numberOfSpaces2 + "s", "");
+                    STDOUT.info("│   {}. --- {}│\n", i, whitespace);
+                }
+            }
+            STDOUT.info("│                                          │\n");
+            STDOUT.info("│                                          │\n");
+            STDOUT.info("│                Page {}/{}                  │\n", pageNumber + 1, numberOfPages);
+            if (numberOfPages > 2 && pageNumber != 0 && pageNumber != numberOfPages - 1) {
+                STDOUT.info("│   P <- Previous page    Next page -> N   │\n");
+            }
+            if (pageNumber == numberOfPages - 1 && numberOfPages>1) {
+                STDOUT.info("│   P <- Previous page                     │\n");
+            }
+            if (pageNumber == 0  && numberOfPages>1) {
+                STDOUT.info("│                         Next page -> N   │\n");
+            }
+            STDOUT.info("└──────────────────────────────────────────┘\n");
+
+            STDOUT.info("\n");
+            STDOUT.info("CHOOSE NUMBER OF DRINK, CHANGE PAGE OR GO TO THE MAIN MENU: ");
+            chooseTheOption();
+        } while (true);
+    }
+
+
+    private void chooseTheOption() {
+        in = SCANNER.next();
+        changePage();
+        quitToMainMenu();
+    }
+
+    private void changePage() {
+        if (in.equalsIgnoreCase("N")) {
+            if (pageNumber == numberOfPages - 1) {
+                STDOUT.info("\n\u001b[31m It's LAST page, you cannot move forward!\u001b[0m\n");
+            } else if (pageNumber < numberOfPages - 1) {
+                pageNumber++;
             }
         }
-        STDOUT.info("│                                          │\n");
-        STDOUT.info("│                  Page {}/{}                │\n", pageNumber + 1, numberOfPages);
-        STDOUT.info("│   P <- Previous page    Next page -> N   │\n");
-        STDOUT.info("└──────────────────────────────────────────┘\n");
-
-        STDOUT.info("\n");
-        STDOUT.info("CHOOSE NUMBER OF DRINK, CHANGE PAGE OR GO TO THE MAIN MENU: ");
-
+        if (in.equalsIgnoreCase("P")) {
+            if (pageNumber == 0) {
+                STDOUT.info("\n\u001b[31m It's FIRST page, you cannot move back!\u001b[0m\n");
+            } else if (pageNumber > 0) {
+                pageNumber--;
+            }
+        }
     }
 
-    public void changePage() {
-
+    private void quitToMainMenu() {
+        if (in.equalsIgnoreCase("X")) {
+            Menu.mainMenu();
+        }
     }
+
+//    public List<Drink> getDrinkByCategory(String category) {
+//        return allDrink
+//                .stream()
+//                .filter(e -> e.getCategory().equalsIgnoreCase(category))
+//                .collect(Collectors.toList());
+
 
     public static void main(String[] args) {
         ListOfDrinks list = new ListOfDrinks();
