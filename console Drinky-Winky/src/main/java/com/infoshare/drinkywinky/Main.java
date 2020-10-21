b88d7059ed66839669bf2b72d2a52f22ba5c3b27
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-10-21 14:01:54 +0200

    Fixed problem

--- console Drinky-Winky/src/main/java/com/infoshare/drinkywinky/Main.java
+++ console Drinky-Winky/src/main/java/com/infoshare/drinkywinky/Main.java
@@ -3,6 +3,7 @@ package com.infoshare.drinkywinky;
 import com.infoshare.drinkywinky.menu.Menu;
 import com.infoshare.drinkywinky.properties.ConfigLoader;
 
+
 public class Main {
 
     public static void main(String[] args) {
