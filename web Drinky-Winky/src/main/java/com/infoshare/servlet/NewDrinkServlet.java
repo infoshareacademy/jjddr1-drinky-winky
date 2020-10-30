7bc53071fd80f52b17a38bda639db52cdee67033
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-10-30 22:21:14 +0100

    working save to database

--- web Drinky-Winky/src/main/java/com/infoshare/servlet/NewDrinkServlet.java
+++ web Drinky-Winky/src/main/java/com/infoshare/servlet/NewDrinkServlet.java
@@ -1,5 +1,9 @@
 package com.infoshare.servlet;
 
+import com.infoshare.dto.DrinkDTO;
+import com.infoshare.model.Category;
+import com.infoshare.model.Drink;
+import com.infoshare.model.Ingredient;
 import com.infoshare.service.DrinkService;
 
 import javax.inject.Inject;
@@ -10,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/NewDrinkServlet")
 public class NewDrinkServlet extends HttpServlet {
@@ -35,6 +41,27 @@ public class NewDrinkServlet extends HttpServlet {
         String ingredient = request.getParameter("ingredient");
         String measure = request.getParameter("measure");
 
+        Category category1 = new Category();
+        category1.setName(category);
 
+        List<Ingredient> ingredient1 = new ArrayList<>();
+        Ingredient ingredient2 = new Ingredient();
+        ingredient2.setName(ingredient);
+        ingredient2.setMeasure(measure);
+        ingredient1.add(ingredient2);
+
+        DrinkDTO drinkDTO = new DrinkDTO();
+        drinkDTO.setName(name);
+        drinkDTO.setCustom(isCustom);
+        drinkDTO.setApproved(isApproved);
+        drinkDTO.setRecipe(recipe);
+        drinkDTO.setDrinkType(drinkType);
+        drinkDTO.setGlassType(glassType);
+        drinkDTO.setModificationDate(modificationDate);
+        drinkDTO.setImageUrl(imageUrl);
+        drinkDTO.setCategory(category1);
+        drinkDTO.setIngredientList(ingredient1);
+
+        drinkService.addDrink(drinkDTO);
     }
 }
