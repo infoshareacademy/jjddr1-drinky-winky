12fc3eca1db8ee93ab3eecceb86064181d51b174
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-11-06 08:51:44 +0100

    getRequestDrinkList method corrected

--- web Drinky-Winky/src/main/java/com/infoshare/servlet/ViewDrinkServlet.java
+++ web Drinky-Winky/src/main/java/com/infoshare/servlet/ViewDrinkServlet.java
@@ -4,6 +4,7 @@ import com.infoshare.dto.DrinkDTO;
 import com.infoshare.freemarker.TemplateProvider;
 import com.infoshare.model.Drink;
 import com.infoshare.service.DrinkService;
+import com.infoshare.utils.Utils;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
@@ -26,18 +27,23 @@ public class ViewDrinkServlet extends HttpServlet {
     TemplateProvider templateProvider;
     @Inject
     DrinkService drinkService;
+    @Inject
+    Utils utils;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 
         Map<String, Object> root = new HashMap<>();
         root.put("names", drinkService.getDrinkList());
+        root.put("boundedList", utils.getRequestDrinkList(3,11));
 
         List<DrinkDTO> recipesList = drinkService.getDrinkList();
 
         for(DrinkDTO drink:recipesList){
             root.put("ing",drink.getIngredientList());
         }
+
+        List<DrinkDTO> requestDrinkList = utils.getRequestDrinkList(1, 5);
 
         Template template = templateProvider.getTemplate(getServletContext(), "view.ftlh");
         Writer out = response.getWriter();
--- web Drinky-Winky/src/main/java/com/infoshare/utils/Utils.java
+++ web Drinky-Winky/src/main/java/com/infoshare/utils/Utils.java
@@ -2,22 +2,35 @@ package com.infoshare.utils;
 
 import com.infoshare.dto.DrinkDTO;
 import com.infoshare.service.DrinkService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import java.util.List;
 
-
+@RequestScoped
 public class Utils {
+    Logger logger = LoggerFactory.getLogger(getClass().getName());
 
     @Inject
     DrinkService drinkService;
 
     public List<DrinkDTO> getRequestDrinkList(int request, int size) {
 
-        int fromIndex = (request - 1) * size;
-        int toIndex = request * size - 1;
+//        int fromIndex = (request - 1) * size;
+//        int toIndex = request * size - 1;
 
-        return drinkService.getDrinkList().subList(fromIndex, toIndex);
+        int fromIndex = request - 1;
+        int toIndex = request + size - 1;
+
+        int drinkDbSize = drinkService.getDrinkList().size();
+
+        if (fromIndex >= 0 && toIndex <= drinkDbSize-1) {
+            return drinkService.getDrinkList().subList(fromIndex, toIndex);
+        } else {
+            logger.info("Database counts {} drinks. Give correct bound numbers", drinkDbSize);
+            return drinkService.getDrinkList().subList(1,1);
+        }
     }
-
 }
