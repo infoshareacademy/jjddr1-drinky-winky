f6d3d1708acba47da71ef8d7c49fd7f8db2f00e2
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-11-08 00:07:05 +0100

    LoggingTestServlet extended

--- web Drinky-Winky/src/main/java/com/infoshare/service/LoggingService.java
+++ web Drinky-Winky/src/main/java/com/infoshare/service/LoggingService.java
@@ -2,9 +2,11 @@ package com.infoshare.service;
 
 import com.infoshare.dto.UserDTO;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import java.util.Optional;
 
+@RequestScoped
 public class LoggingService {
 
     @Inject
@@ -12,6 +14,10 @@ public class LoggingService {
 
     public Optional<UserDTO> checkIfUserExist(String login) {
         return Optional.ofNullable(userService.findUserByLogin(login));
+    }
+
+    public boolean checkCorrectPassword(UserDTO userDTO, String password) {
+        return userDTO.getPassword().equals(password);
     }
 
     public void checkIfUserIsSigned() {
--- web Drinky-Winky/src/main/java/com/infoshare/servlet/LoggingTestServlet.java
+++ web Drinky-Winky/src/main/java/com/infoshare/servlet/LoggingTestServlet.java
@@ -1,7 +1,10 @@
 package com.infoshare.servlet;
 
+import com.infoshare.dto.UserDTO;
 import com.infoshare.model.User;
+import com.infoshare.service.LoggingService;
 
+import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,20 +18,36 @@ import java.io.PrintWriter;
 @WebServlet("/logpage")
 public class LoggingTestServlet extends HttpServlet {
 
+    @Inject
+    LoggingService loggingService;
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         RequestDispatcher rd = request.getRequestDispatcher("logging.jsp");
-        rd.forward(request,response);
+        rd.forward(request, response);
 
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User user = new User();
-        user.setLogin(request.getParameter("login"));
-        user.setPassword(request.getParameter("password"));
+
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
 
         PrintWriter printWriter = response.getWriter();
-        printWriter.write("<!DOCTYPE html><html><body>"+user.getLogin()+ " " + user.getPassword() + "</body></html>");
 
+        if (loggingService.checkIfUserExist(login).isPresent()) {
+            UserDTO registeredUserDTO = loggingService.checkIfUserExist(login).get();
+
+            if (loggingService.checkCorrectPassword(registeredUserDTO, password)) {
+                request.getSession().setAttribute("login", registeredUserDTO.getLogin());
+                request.getSession().setAttribute("password", registeredUserDTO.getPassword());
+
+                printWriter.write("<!DOCTYPE html><html><body>You're logged in</br>" + registeredUserDTO.getLogin() + " " + registeredUserDTO.getPassword() + "</body></html>");
+            } else {
+                printWriter.write("<!DOCTYPE html><html><body>Write correct password</body></html>");
+            }
+        } else {
+            printWriter.write("<!DOCTYPE html><html><body>User with that login doesn't exist. Please register!</body></html>");
+        }
     }
 }
