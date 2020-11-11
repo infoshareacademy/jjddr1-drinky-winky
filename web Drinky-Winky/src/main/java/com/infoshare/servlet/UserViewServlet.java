a4e65f4b6e78cdc17e6448704a75cd0cc466d870
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2020-11-11 18:01:14 +0100

    refactor in userviewserv

--- web Drinky-Winky/src/main/java/com/infoshare/servlet/UserViewServlet.java
+++ web Drinky-Winky/src/main/java/com/infoshare/servlet/UserViewServlet.java
@@ -5,20 +5,15 @@ import com.infoshare.service.CategoryService;
 import com.infoshare.service.DrinkService;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import org.w3c.dom.stylesheets.LinkStyle;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @WebServlet("/User-view")
 public class UserViewServlet extends HttpServlet {
@@ -32,52 +27,53 @@ public class UserViewServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        Map<String, Object> root = new HashMap<>();
 
         HttpSession session = request.getSession(true);
-        session.setMaxInactiveInterval(5 * 60);
+        session.setMaxInactiveInterval(10);
         Object login = session.getAttribute("login");
         if (login == null) {
             response.sendRedirect("Logout");
         }
+            Map<String, Object> root = new HashMap<>();
 
-        int size = drinkService.getDrinkList().size();
-        int numberOfPage = 1;
-        List<Integer> pages = new ArrayList<>();
+            int size = drinkService.getDrinkList().size();
+            int numberOfPage = 1;
+            List<Integer> pages = new ArrayList<>();
 
-        if (size != 0) {
-            if (size % 8 == 0) {
-                numberOfPage = size / 8;
+            if (size != 0) {
+                if (size % 8 == 0) {
+                    numberOfPage = size / 8;
+                } else {
+                    numberOfPage = size / 8 + 1;
+                }
+            }
+
+            for (int pageNumber = 1; pageNumber <= numberOfPage; pageNumber++) {
+                pages.add(pageNumber);
+            }
+
+            String page = request.getParameter("page");
+            if (page.equals("user")) {
+                root.put("drinkList", drinkService.getRequestDrinkList(numberOfPage, 8));
+                root.put("allDrink", drinkService.getDrinkList());
+                root.put("categories", categoryService.getCategoriesList());
             } else {
-                numberOfPage = size / 8 + 1;
+                int i = Integer.parseInt(page);
+                root.put("allDrink", drinkService.getDrinkList());
+                root.put("drinkList", drinkService.getRequestDrinkList(i, 8));
+                root.put("categories", categoryService.getCategoriesList());
+                root.put("pageNumber", pages);
+            }
+
+
+            Template template = templateProvider.getTemplate(getServletContext(), "user-view.ftlh");
+            Writer out = response.getWriter();
+
+            try {
+                template.process(root, out);
+            } catch (TemplateException e) {
+                e.printStackTrace();
             }
         }
-
-        for (int pageNumber = 1; pageNumber <= numberOfPage; pageNumber++) {
-            pages.add(pageNumber);
-        }
-
-        String page = request.getParameter("page");
-        if (page.equals("user")) {
-            root.put("drinkList", drinkService.getRequestDrinkList(numberOfPage, 8));
-            root.put("allDrink", drinkService.getDrinkList());
-            root.put("categories", categoryService.getCategoriesList());
-        } else {
-            int i = Integer.parseInt(page);
-            root.put("allDrink", drinkService.getDrinkList());
-            root.put("drinkList", drinkService.getRequestDrinkList(i, 8));
-            root.put("categories", categoryService.getCategoriesList());
-            root.put("pageNumber", pages);
-        }
-
-
-        Template template = templateProvider.getTemplate(getServletContext(), "user-view.ftlh");
-        Writer out = response.getWriter();
-
-        try {
-            template.process(root, out);
-        } catch (TemplateException e) {
-            e.printStackTrace();
-        }
     }
-}
+
