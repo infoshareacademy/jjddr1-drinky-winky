fe09ddb74a047ea9b5cf429d37cee669b0d7eba7
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-11-19 15:04:07 +0100

    Viewing real name of the user in user-view interface

--- web Drinky-Winky/src/main/java/com/infoshare/servlet/UserViewServlet.java
+++ web Drinky-Winky/src/main/java/com/infoshare/servlet/UserViewServlet.java
@@ -1,8 +1,10 @@
 package com.infoshare.servlet;
 
+import com.infoshare.dto.UserDTO;
 import com.infoshare.freemarker.TemplateProvider;
 import com.infoshare.service.CategoryService;
 import com.infoshare.service.DrinkService;
+import com.infoshare.service.UserService;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
@@ -15,10 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Supplier;
 
 @WebServlet("/User-view")
 public class UserViewServlet extends HttpServlet {
@@ -29,6 +29,8 @@ public class UserViewServlet extends HttpServlet {
     DrinkService drinkService;
     @Inject
     CategoryService categoryService;
+    @Inject
+    UserService userService;
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -71,7 +73,10 @@ public class UserViewServlet extends HttpServlet {
             root.put("categories", categoryService.getCategoriesList());
             root.put("pageNumber", pages);
         }
+        String loginUser = (String) request.getSession().getAttribute("login");
 
+        String loggedUser = userService.findUserByLogin(loginUser).orElseThrow().getName();
+        root.put("loggedUser", loggedUser);
 
         Template template = templateProvider.getTemplate(getServletContext(), "user-view.ftlh");
         Writer out = response.getWriter();
