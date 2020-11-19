ae38c6dedf415c33c4ab1171caf4aeb4789f3a88
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-11-19 17:00:08 +0100

    add fav list for another users, fix message

--- web Drinky-Winky/src/main/java/com/infoshare/servlet/AddFavouriteServlet.java
+++ web Drinky-Winky/src/main/java/com/infoshare/servlet/AddFavouriteServlet.java
@@ -1,5 +1,6 @@
 package com.infoshare.servlet;
 
+import com.infoshare.dto.UserDTO;
 import com.infoshare.service.DrinkService;
 import com.infoshare.service.UserService;
 
@@ -9,7 +10,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet("/Add-favourite")
 public class AddFavouriteServlet extends HttpServlet {
@@ -22,11 +25,12 @@ public class AddFavouriteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 
+        HttpSession session = request.getSession(true);
+        String login = (String) session.getAttribute("login");
+        Long id = userService.findUserByLogin(login).orElseThrow().getId();
         String name = request.getParameter("name");
-
         Long drinkId = drinkService.getDrinkByName(name).getId();
-
-        userService.saveFavDrink(drinkId, 1L);
+        userService.saveFavDrink(drinkId, id);
 
         response.sendRedirect("Drink-view?name=" + name);
     }
--- web Drinky-Winky/src/main/java/com/infoshare/servlet/DrinkViewServlet.java
+++ web Drinky-Winky/src/main/java/com/infoshare/servlet/DrinkViewServlet.java
@@ -4,6 +4,7 @@ import com.infoshare.dto.DrinkDTO;
 import com.infoshare.freemarker.TemplateProvider;
 import com.infoshare.model.Drink;
 import com.infoshare.service.DrinkService;
+import com.infoshare.service.MessageService;
 import com.infoshare.service.UserService;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -14,6 +15,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.HashMap;
@@ -29,9 +31,15 @@ public class DrinkViewServlet extends HttpServlet {
     DrinkService drinkService;
     @Inject
     UserService userService;
+    @Inject
+    MessageService messageService;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+
+        HttpSession session = request.getSession(true);
+        String login = (String) session.getAttribute("login");
+        Long id = userService.findUserByLogin(login).orElseThrow().getId();
 
         DrinkDTO drink = drinkService.getDrinkByName(request.getParameter("name"));
 
@@ -40,7 +48,12 @@ public class DrinkViewServlet extends HttpServlet {
         root.put("ingredients",drink.getIngredientList());
         root.put("user",request.getRemoteUser());
         root.put("allDrink", drinkService.getDrinkList());
-        root.put("favourite", userService.isFavourite(drink.getName(), 1L));
+        root.put("favourite", userService.isFavourite(drink.getName(), id));
+
+        if (messageService.getMessageById(1L).isPresent()) {
+            root.put("message", messageService.getMessageById(1L).orElseThrow());
+            messageService.clean(1L);
+        }
 
         Template template = templateProvider.getTemplate(getServletContext(), "drink-view.ftlh");
         Writer out = response.getWriter();
